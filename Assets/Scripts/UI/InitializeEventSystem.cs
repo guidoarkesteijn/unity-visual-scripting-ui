b0873dd55cc140ab48296bebb21bc0bce11f5be1
Guido Arkesteijn <guidoarkesteijn@gmail.com>
2021-01-31 01:57:59 +0100

    Cleanup and moving some things to separate assets

---
+++ Assets/Scripts/UI/InitializeEventSystem.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public static class InitializeEventSystem
+{
+    [RuntimeInitializeOnLoadMethod]
+    static void Initialize()
+    {
+        GameObject go = new GameObject("EventSystem", typeof(EventSystem), typeof(StandaloneInputModule));
+        Object.DontDestroyOnLoad(go);
+    }
+}
